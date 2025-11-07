549cc299b2d9ae1a12cc558de7fde7bf95b5109f
BarlomiejKrol4429 <bartlomiej.krol@uczen.mechaniktg.pl>
2025-11-07 11:57:17 +0100

    Zdefiniowanie klasy Zdjecie

--- app/src/main/java/com/example/polubienia/Zdjecie.java
+++ app/src/main/java/com/example/polubienia/Zdjecie.java
@@ -1,4 +1,32 @@
 package com.example.polubienia;
 
 public class Zdjecie {
+    private String src;
+    private Double ocena;
+
+    public Zdjecie(String src, Double ocena) {
+        this.src = src;
+        this.ocena = ocena;
+    }
+
+    public String getSrc() {
+        return src;
+    }
+
+    public void setSrc(String src) {
+        this.src = src;
+    }
+
+    public Double getOcena() {
+        return ocena;
+    }
+
+    public void setOcena(Double ocena) {
+        this.ocena = ocena;
+    }
+
+    @Override
+    public String toString() {
+        return src + " " + ocena;
+    }
 }
