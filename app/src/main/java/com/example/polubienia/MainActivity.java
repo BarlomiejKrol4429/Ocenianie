fdd456e48f249cb9308c04a1180280f683540762
BarlomiejKrol4429 <bartlomiej.krol@uczen.mechaniktg.pl>
2025-11-07 12:07:55 +0100

    Funkcjonalność przycisków (1/2)

--- app/src/main/java/com/example/polubienia/MainActivity.java
+++ app/src/main/java/com/example/polubienia/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.polubienia;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView image_view;
     private ArrayList<Zdjecie> lista;
     private RatingBar rating_bar;
+    private int wybrany_obrazek = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +38,40 @@ public class MainActivity extends AppCompatActivity {
         image_view = findViewById(R.id.imageView);
         lista = new ArrayList<>();
         rating_bar = findViewById(R.id.ratingBar);
+
+        Zdjecie zdjecie1 = new Zdjecie("R.drawable.kot1", 0.0);
+        Zdjecie zdjecie2 = new Zdjecie("R.drawable.kot2", 0.0);
+        Zdjecie zdjecie3 = new Zdjecie("R.drawable.kot3", 0.0);
+        Zdjecie zdjecie4 = new Zdjecie("R.drawable.kot4", 0.0);
+        lista.add(zdjecie1);
+        lista.add(zdjecie2);
+        lista.add(zdjecie3);
+        lista.add(zdjecie4);
+
+        button_prev.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        wybrany_obrazek --;
+                        if(wybrany_obrazek == -1){
+                            wybrany_obrazek = lista.size()-1;
+                        }
+                        image_view.setImageResource(lista.get(wybrany_obrazek));
+                    }
+                }
+        );
+        button_next.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        wybrany_obrazek ++;
+                        if(wybrany_obrazek > lista.size()-1){
+                            wybrany_obrazek = 0;
+                        }
+                        image_view.setImageResource(lista.get(wybrany_obrazek));
+                    }
+                }
+        );
 
     }
 }
